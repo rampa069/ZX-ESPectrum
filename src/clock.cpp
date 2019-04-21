1c2714a2efbfcc28d2733a41b784468d1521692e
Ramón Martínez <rampa@encomix.org>
2019-04-21 12:22:08 +0200

    clock.c cleanup

--- src/clock.cpp
+++ src/clock.cpp
@@ -5,25 +5,14 @@
 
 #define CPU_SPEED 3580000
 #define CPU_SPEED_MHZ 3.58
-#define FRAME_PERIOD_MS 20 // or 17??
-
-
-int IFreq = 100;
-int CPUSpeed=500;
-
-
+#define FRAME_PERIOD_MS 20
 
 
 void setup_cpuspeed() {
-
-
-  Z80_IPeriod = 20000; //Z80_IPeriod=(3579545*CPUSpeed)/(IFreq*1000);;
-  Z80_ICount =  60000;
   int calculated_count = CPU_SPEED_MHZ * FRAME_PERIOD_MS * 1000;
   int calculated_period = FRAME_PERIOD_MS * 1000;
   Z80_IPeriod=calculated_period;
   Z80_ICount= calculated_count;
   Serial.printf ("calculated ICount: %d\n",calculated_count);
   Serial.printf ("calculated IPeriod: %d\n",calculated_period);
-
 }
