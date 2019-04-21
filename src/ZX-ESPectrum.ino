a16fbf826a262428544d7d6ef43848b31694c58f
jorge.fuertes <jorge@jorgefuertes.com>
2019-04-21 12:24:33 +0200

    Code clean

--- src/ZX-ESPectrum.ino
+++ src/ZX-ESPectrum.ino
@@ -61,64 +61,64 @@ SemaphoreHandle_t xMutex;
 
 void setup()
 {
-  // Turn off peripherals to gain memory (?do they release properly)
-  esp_bt_controller_deinit();
-  esp_bt_controller_mem_release(ESP_BT_MODE_BTDM);
-  //esp_wifi_set_mode(WIFI_MODE_NULL);
+        // Turn off peripherals to gain memory (?do they release properly)
+        esp_bt_controller_deinit();
+        esp_bt_controller_mem_release(ESP_BT_MODE_BTDM);
+        //esp_wifi_set_mode(WIFI_MODE_NULL);
 
-  Serial.begin(115200);
-  Serial.println("CHIP setup.");
-  Serial.println("VGA framebufer");
+        Serial.begin(115200);
+        Serial.println("CHIP setup.");
+        Serial.println("VGA framebufer");
 
-  //we need double buffering for smooth animations
-  vga.setFrameBufferCount(2);
-  Serial.println("VGA init");
-  vga.init(vga.MODE360x200 , redPin, greenPin, bluePin, hsyncPin, vsyncPin);
-  Serial.println("VGA initialized");
+        //we need double buffering for smooth animations
+        vga.setFrameBufferCount(2);
+        Serial.println("VGA init");
+        vga.init(vga.MODE360x200, redPin, greenPin, bluePin, hsyncPin, vsyncPin);
+        Serial.println("VGA initialized");
 
-  pinMode(SOUND_PIN,OUTPUT);
-  digitalWrite(SOUND_PIN,LOW);
+        pinMode(SOUND_PIN,OUTPUT);
+        digitalWrite(SOUND_PIN,LOW);
 
-  start_im1_irq=1;
+        start_im1_irq=1;
 
-  kb_begin();
+        kb_begin();
 
-  // ALLOCATE MEMORY
-  //
-  bank0 = (byte *)malloc(49152);
-  if(bank0 == NULL)Serial.println("Failed to allocate Bank 1 memory");
-  Serial.printf("Free Heap after bank0: %d\n",system_get_free_heap_size());
+        // ALLOCATE MEMORY
+        //
+        bank0 = (byte *)malloc(49152);
+        if(bank0 == NULL) Serial.println("Failed to allocate Bank 1 memory");
+        Serial.printf("Free Heap after bank0: %d\n",system_get_free_heap_size());
 
-  setup_cpuspeed();
+        setup_cpuspeed();
 
-  // START Z80
+        // START Z80
 
-  Serial.println("RESETTING Z80");
-  Z80_Reset();
+        Serial.println("RESETTING Z80");
+        Z80_Reset();
 
-  // make sure keyboard ports are FF
-  for(int t = 0;t < 32;t++)
-  {
-    z80ports_in[t] = 0xff;
-  }
+        // make sure keyboard ports are FF
+        for(int t = 0; t < 32; t++)
+        {
+                z80ports_in[t] = 0xff;
+        }
 
-  Serial.printf("Setup: MAIN Executing on core %x ",xPortGetCoreID());
-  Serial.printf("Free Heap after Z80 Reset: %d\n",system_get_free_heap_size());
+        Serial.printf("Setup: MAIN Executing on core %x ",xPortGetCoreID());
+        Serial.printf("Free Heap after Z80 Reset: %d\n",system_get_free_heap_size());
 
-  xMutex = xSemaphoreCreateMutex();
+        xMutex = xSemaphoreCreateMutex();
 
-  xTaskCreatePinnedToCore(
-                      videoTask,   /* Function to implement the task */
-                      "videoTask", /* Name of the task */
-                      2048,      /* Stack size in words */
-                      NULL,       /* Task input parameter */
-                      20,          /* Priority of the task */
-                      NULL,       /* Task handle. */
-                      0);  /* Core where the task should run */
+        xTaskCreatePinnedToCore(
+                videoTask,         /* Function to implement the task */
+                "videoTask",       /* Name of the task */
+                2048,            /* Stack size in words */
+                NULL,             /* Task input parameter */
+                20,                /* Priority of the task */
+                NULL,             /* Task handle. */
+                0);        /* Core where the task should run */
 
 
-  if (run_snapshot)
-     load_speccy();
+        if (run_snapshot)
+                load_speccy();
 }
 
 
@@ -127,62 +127,62 @@ void setup()
 
 void videoTask( void * parameter )
 {
-   unsigned int ff,i,byte_offset;
-   unsigned char color_attrib,pixel_map,zx_fore_color,zx_back_color,flash,bright;
-   unsigned int zx_vidcalc;
-   unsigned int tmpColour;
+        unsigned int ff,i,byte_offset;
+        unsigned char color_attrib,pixel_map,zx_fore_color,zx_back_color,flash,bright;
+        unsigned int zx_vidcalc;
+        unsigned int tmpColour;
 
-   while(1)
-   {
-        xSemaphoreTake( xMutex, portMAX_DELAY );
-
-        if (flashing++ > 20)
-            flashing=0;
-
-        vga.clear(zxcolor(borderTemp,0));
-        for(unsigned int lin = 0;lin < 192;lin++)
+        while(1)
         {
-          for(ff=0;ff<32;ff++)  //foreach byte in line
-          {
-            byte_offset=lin*32+ff;//*2+1;
-            //spectrum attributes
-            //bit 7 6   5 4 3   2 1 0
-            //    F B   P A P   I N K
-            color_attrib=bank0[0x1800+(calcY(byte_offset)/8)*32+ff];//get 1 of 768 attrib values
-            pixel_map=bank0[byte_offset];
-            zx_fore_color=color_attrib & 0x07;
-            zx_back_color=(color_attrib & 0x38)>>3;
+                xSemaphoreTake( xMutex, portMAX_DELAY );
 
-            flash=bitRead(color_attrib,7);
-            bright=bitRead(color_attrib,6);
+                if (flashing++ > 20)
+                        flashing=0;
 
-            if (flash){
-              if (flashing > 10) {
-                  tmpColour=zx_fore_color;
-                  zx_fore_color=zx_back_color;
-                  zx_back_color=tmpColour;
-              }
-            }
+                vga.clear(zxcolor(borderTemp,0));
+                for(unsigned int lin = 0; lin < 192; lin++)
+                {
+                        for(ff=0; ff<32; ff++) //foreach byte in line
+                        {
+                                byte_offset=lin*32+ff;//*2+1;
+                                //spectrum attributes
+                                //bit 7 6   5 4 3   2 1 0
+                                //    F B   P A P   I N K
+                                color_attrib=bank0[0x1800+(calcY(byte_offset)/8)*32+ff];//get 1 of 768 attrib values
+                                pixel_map=bank0[byte_offset];
+                                zx_fore_color=color_attrib & 0x07;
+                                zx_back_color=(color_attrib & 0x38)>>3;
+
+                                flash=bitRead(color_attrib,7);
+                                bright=bitRead(color_attrib,6);
+
+                                if (flash) {
+                                        if (flashing > 10) {
+                                                tmpColour=zx_fore_color;
+                                                zx_fore_color=zx_back_color;
+                                                zx_back_color=tmpColour;
+                                        }
+                                }
 
 
 
-            for(i=0;i<8;i++)  //foreach pixel within a byte
-            {
-                zx_vidcalc=ff*8+i;
-                byte bitpos = (0x80 >> i);
-                if((pixel_map & bitpos)!=0)
-                               vga.dotFast(zx_vidcalc+52, calcY(byte_offset)+5,zxcolor(zx_fore_color,bright)) ;
-                else
-                               vga.dotFast(zx_vidcalc+52, calcY(byte_offset)+5,zxcolor(zx_back_color,bright)) ;
+                                for(i=0; i<8; i++) //foreach pixel within a byte
+                                {
+                                        zx_vidcalc=ff*8+i;
+                                        byte bitpos = (0x80 >> i);
+                                        if((pixel_map & bitpos)!=0)
+                                                vga.dotFast(zx_vidcalc+52, calcY(byte_offset)+5,zxcolor(zx_fore_color,bright));
+                                        else
+                                                vga.dotFast(zx_vidcalc+52, calcY(byte_offset)+5,zxcolor(zx_back_color,bright));
 
-            }
-          }
+                                }
+                        }
+                }
+                vga.show();
+
+                xSemaphoreGive( xMutex );
+                vTaskDelay(1);
         }
-         vga.show();
-
-        xSemaphoreGive( xMutex );
-        vTaskDelay(1) ;
-     }
 }
 
 
@@ -192,42 +192,42 @@ void videoTask( void * parameter )
 //
 /* Calculate Y coordinate (0-192) from Spectrum screen memory location */
 int calcY(int offset){
-  return ((offset>>11)<<6)        // sector start
-       +((offset%2048)>>8)        // pixel rows
-       +((((offset%2048)>>5)-((offset%2048)>>8<<3))<<3);  // character rows
+        return ((offset>>11)<<6)  // sector start
+               +((offset%2048)>>8) // pixel rows
+               +((((offset%2048)>>5)-((offset%2048)>>8<<3))<<3); // character rows
 }
 
 /* Calculate X coordinate (0-255) from Spectrum screen memory location */
 int calcX(int offset){
-  return (offset%32)<<3;
+        return (offset%32)<<3;
 }
 
 
 unsigned int zxcolor(byte c,byte f)
 {
-  int rgbLevel=192;
-  if (f)
-    rgbLevel=255;
-  else
-    rgbLevel=192;
+        int rgbLevel=192;
+        if (f)
+                rgbLevel=255;
+        else
+                rgbLevel=192;
 
-  if(c == 0)
-    return vga.RGB(0,0,0);
-  else if (c == 1)
-    return vga.RGB(0,0,rgbLevel);
-  else if (c == 2)
-    return vga.RGB(rgbLevel,0,0);
-  else if (c == 3)
-    return vga.RGB(rgbLevel,0,rgbLevel);
-  else if (c == 4)
-    return vga.RGB(0,rgbLevel,0);
-  else if (c == 5)
-    return vga.RGB(0,rgbLevel,rgbLevel);
-  else if (c == 6)
-    return vga.RGB(rgbLevel,rgbLevel,0);
-  else if (c == 7)
-    return vga.RGB(rgbLevel,rgbLevel,rgbLevel);
-  return 0;
+        if(c == 0)
+                return vga.RGB(0,0,0);
+        else if (c == 1)
+                return vga.RGB(0,0,rgbLevel);
+        else if (c == 2)
+                return vga.RGB(rgbLevel,0,0);
+        else if (c == 3)
+                return vga.RGB(rgbLevel,0,rgbLevel);
+        else if (c == 4)
+                return vga.RGB(0,rgbLevel,0);
+        else if (c == 5)
+                return vga.RGB(0,rgbLevel,rgbLevel);
+        else if (c == 6)
+                return vga.RGB(rgbLevel,rgbLevel,0);
+        else if (c == 7)
+                return vga.RGB(rgbLevel,rgbLevel,rgbLevel);
+        return 0;
 }
 
 
@@ -236,68 +236,68 @@ unsigned int zxcolor(byte c,byte f)
 
 void loop()
 {
-  while (1)
-  {
-        Z80();
-        start_im1_irq=1;    // keyboard scan is run in IM1 interrupt
-        vTaskDelay(1) ;  //important to avoid task watchdog timeouts - change this to slow down emu
-  }
+        while (1)
+        {
+                Z80();
+                start_im1_irq=1; // keyboard scan is run in IM1 interrupt
+                vTaskDelay(1); //important to avoid task watchdog timeouts - change this to slow down emu
+        }
 }
 
 
 void do_keyboard()
 {
 
-    if (keymap != oldKeymap)
-    {
-      bitWrite(z80ports_in[0], 0, keymap[0x12]);
-      bitWrite(z80ports_in[0], 1, keymap[0x1a]);
-      bitWrite(z80ports_in[0], 2, keymap[0x22]);
-      bitWrite(z80ports_in[0], 3, keymap[0x21]);
-      bitWrite(z80ports_in[0], 4, keymap[0x2a]);
+        if (keymap != oldKeymap)
+        {
+                bitWrite(z80ports_in[0], 0, keymap[0x12]);
+                bitWrite(z80ports_in[0], 1, keymap[0x1a]);
+                bitWrite(z80ports_in[0], 2, keymap[0x22]);
+                bitWrite(z80ports_in[0], 3, keymap[0x21]);
+                bitWrite(z80ports_in[0], 4, keymap[0x2a]);
 
-      bitWrite(z80ports_in[1], 0, keymap[0x1c]);
-      bitWrite(z80ports_in[1], 1, keymap[0x1b]);
-      bitWrite(z80ports_in[1], 2, keymap[0x23]);
-      bitWrite(z80ports_in[1], 3, keymap[0x2b]);
-      bitWrite(z80ports_in[1], 4, keymap[0x34]);
+                bitWrite(z80ports_in[1], 0, keymap[0x1c]);
+                bitWrite(z80ports_in[1], 1, keymap[0x1b]);
+                bitWrite(z80ports_in[1], 2, keymap[0x23]);
+                bitWrite(z80ports_in[1], 3, keymap[0x2b]);
+                bitWrite(z80ports_in[1], 4, keymap[0x34]);
 
-      bitWrite(z80ports_in[2], 0, keymap[0x15]);
-      bitWrite(z80ports_in[2], 1, keymap[0x1d]);
-      bitWrite(z80ports_in[2], 2, keymap[0x24]);
-      bitWrite(z80ports_in[2], 3, keymap[0x2d]);
-      bitWrite(z80ports_in[2], 4, keymap[0x2c]);
+                bitWrite(z80ports_in[2], 0, keymap[0x15]);
+                bitWrite(z80ports_in[2], 1, keymap[0x1d]);
+                bitWrite(z80ports_in[2], 2, keymap[0x24]);
+                bitWrite(z80ports_in[2], 3, keymap[0x2d]);
+                bitWrite(z80ports_in[2], 4, keymap[0x2c]);
 
-      bitWrite(z80ports_in[3], 0, keymap[0x16]);
-      bitWrite(z80ports_in[3], 1, keymap[0x1e]);
-      bitWrite(z80ports_in[3], 2, keymap[0x26]);
-      bitWrite(z80ports_in[3], 3, keymap[0x25]);
-      bitWrite(z80ports_in[3], 4, keymap[0x2e]);
+                bitWrite(z80ports_in[3], 0, keymap[0x16]);
+                bitWrite(z80ports_in[3], 1, keymap[0x1e]);
+                bitWrite(z80ports_in[3], 2, keymap[0x26]);
+                bitWrite(z80ports_in[3], 3, keymap[0x25]);
+                bitWrite(z80ports_in[3], 4, keymap[0x2e]);
 
-      bitWrite(z80ports_in[4], 0, keymap[0x45]);
-      bitWrite(z80ports_in[4], 1, keymap[0x46]);
-      bitWrite(z80ports_in[4], 2, keymap[0x3e]);
-      bitWrite(z80ports_in[4], 3, keymap[0x3d]);
-      bitWrite(z80ports_in[4], 4, keymap[0x36]);
+                bitWrite(z80ports_in[4], 0, keymap[0x45]);
+                bitWrite(z80ports_in[4], 1, keymap[0x46]);
+                bitWrite(z80ports_in[4], 2, keymap[0x3e]);
+                bitWrite(z80ports_in[4], 3, keymap[0x3d]);
+                bitWrite(z80ports_in[4], 4, keymap[0x36]);
 
-      bitWrite(z80ports_in[5], 0, keymap[0x4d]);
-      bitWrite(z80ports_in[5], 1, keymap[0x44]);
-      bitWrite(z80ports_in[5], 2, keymap[0x43]);
-      bitWrite(z80ports_in[5], 3, keymap[0x3c]);
-      bitWrite(z80ports_in[5], 4, keymap[0x35]);
+                bitWrite(z80ports_in[5], 0, keymap[0x4d]);
+                bitWrite(z80ports_in[5], 1, keymap[0x44]);
+                bitWrite(z80ports_in[5], 2, keymap[0x43]);
+                bitWrite(z80ports_in[5], 3, keymap[0x3c]);
+                bitWrite(z80ports_in[5], 4, keymap[0x35]);
 
-      bitWrite(z80ports_in[6], 0, keymap[0x5a]);
-      bitWrite(z80ports_in[6], 1, keymap[0x4b]);
-      bitWrite(z80ports_in[6], 2, keymap[0x42]);
-      bitWrite(z80ports_in[6], 3, keymap[0x3b]);
-      bitWrite(z80ports_in[6], 4, keymap[0x33]);
+                bitWrite(z80ports_in[6], 0, keymap[0x5a]);
+                bitWrite(z80ports_in[6], 1, keymap[0x4b]);
+                bitWrite(z80ports_in[6], 2, keymap[0x42]);
+                bitWrite(z80ports_in[6], 3, keymap[0x3b]);
+                bitWrite(z80ports_in[6], 4, keymap[0x33]);
 
-      bitWrite(z80ports_in[7], 0, keymap[0x29]);
-      bitWrite(z80ports_in[7], 1, keymap[0x14]);
-      bitWrite(z80ports_in[7], 2, keymap[0x3a]);
-      bitWrite(z80ports_in[7], 3, keymap[0x31]);
-      bitWrite(z80ports_in[7], 4, keymap[0x32]);
-    }
-    strcpy(oldKeymap,keymap);
+                bitWrite(z80ports_in[7], 0, keymap[0x29]);
+                bitWrite(z80ports_in[7], 1, keymap[0x14]);
+                bitWrite(z80ports_in[7], 2, keymap[0x3a]);
+                bitWrite(z80ports_in[7], 3, keymap[0x31]);
+                bitWrite(z80ports_in[7], 4, keymap[0x32]);
+        }
+        strcpy(oldKeymap,keymap);
 
 }
--- src/clock.cpp
+++ src/clock.cpp
@@ -1,18 +1,15 @@
-
 #include "Arduino.h"
 #include "Z80.h"
-
 
 #define CPU_SPEED 3580000
 #define CPU_SPEED_MHZ 3.58
 #define FRAME_PERIOD_MS 20
 
-
-void setup_cpuspeed() {
-  int calculated_count = CPU_SPEED_MHZ * FRAME_PERIOD_MS * 1000;
+void setup_cpuspeed()
+{
+  int calculated_count  = CPU_SPEED_MHZ * FRAME_PERIOD_MS * 1000;
   int calculated_period = FRAME_PERIOD_MS * 1000;
-  Z80_IPeriod=calculated_period;
-  Z80_ICount= calculated_count;
-  Serial.printf ("calculated ICount: %d\n",calculated_count);
-  Serial.printf ("calculated IPeriod: %d\n",calculated_period);
+  Z80_IPeriod = calculated_period;
+  Z80_ICount  = calculated_count;
+  Serial.printf ("Calculated ICount: %d, IPeriod: %d\n", calculated_count, calculated_period);
 }
