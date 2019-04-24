51a26dedfba75df190070c91690f31b6d579df30
jorge.fuertes <jorge@jorgefuertes.com>
2019-04-24 23:15:50 +0200

    OSD Square

--- src/osd.ino
+++ src/osd.ino
@@ -1,5 +1,12 @@
 // On Screen Display
 
+static int osd_x = 40;
+static int osd_y = 30;
+static int osd_w = 275;
+static int osd_h = 120;
+static int osd_bg = vga.RGB(0, 100, 200);
+static int osd_fg = vga.RGB(200, 200, 200);
+
 void do_OSD() {
     if (keymap != oldKeymap) {
         /*
@@ -13,16 +20,21 @@ void do_OSD() {
                 delay(5);
             }
             log("OSD ON");
-            vga.clear(7);
-            vga.rect(20, 20, 240, 190, vga.RGB(0, 192, 192));
+            vga.fillRect(40, 30, 275, 120, vga.RGB(0, 100, 200));
+            vga.rect(40, 30, 275, 120, vga.RGB(200, 200, 200));
             vga.show();
             keymap[0x05] = 1;
             while (keymap[0x05] != 0) {
-                log("OSD active...");
+                delay(5);
             }
             log("OSD OFF");
             keymap[0x05] = 1;
             xULAStop = false;
         }
     }
+}
+
+void osdSquare() {
+    vga.fillRect(osd_x, osd_y, osd_w, osd_h, osd_bg);
+    vga.rect(osd_x, osd_y, osd_w, osd_h, osd_fg);
 }
