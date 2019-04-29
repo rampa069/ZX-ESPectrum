85b6e70f7bdbc1d16b49e096d87ae64f3ce5a5ea
jorge.fuertes <jorge@jorgefuertes.com>
2019-04-29 16:46:11 +0200

    No more menu flickering

--- include/Emulator/z80main.h
+++ include/Emulator/z80main.h
@@ -1,10 +1,8 @@
 #ifndef __ZXMAIN_INCLUDED__
 #define __ZXMAIN_INCLUDED__
 
-#include "z80user.h"
-#include "main_ROM.h"
 #include "Sound/ay3-8912-state.h"
-
+#include "z80user.h"
 
 extern Sound::Ay3_8912_state _ay3_8912;
 extern Z80_STATE _zxCpu;
