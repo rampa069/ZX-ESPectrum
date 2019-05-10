bf49d87c7b1bfd74dde87e574bb5cb9df02b14b5
jorge.fuertes <jorge@jorgefuertes.com>
2019-05-10 10:11:52 +0200

    Populate data script

---
+++ scripts/populate-data.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ "$(basename $(pwd))" == "scripts" ]]
+then
+    pushd .. &> /dev/null
+fi
+
+if [[ -d data/.git ]]
+then
+    echo "Data dir exists and its a git repo"
+    pushd data &> /dev/null
+    git pull
+    popd &> /dev/null
+elif [[ -d data ]]
+then
+    echo "Data dir exists but its not a git repo, please remove it."
+    popd &> /dev/null
+    exit 1
+else
+    echo "Cloning data"
+    git clone gogs@git.martianoids.com:imsai/zxespectrum-data.git data
+fi
+
+popd &> /dev/null
+exit 0
