b71cb44ddec6c96e3887d3ebd54c68d2e4899137
jorge.fuertes <jorge@jorgefuertes.com>
2019-05-19 12:01:08 +0200

    Cleaned

--- include/MartianVGA.h
+++ include/MartianVGA.h
@@ -4,11 +4,3 @@
 #include "VGA/VGA14BitI.h"
 #include "VGA/VGA3Bit.h"
 #include "VGA/VGA3BitI.h"
-//#include "Graphics/Font.h"
-// #include <Graphics/Sprites.h>
-// #include <Graphics/Mesh.h>
-// #include <Graphics/Sprites.h>
-// #include <Graphics/Animation.h>
-
-//extern const unsigned char Font6x8Pixels[];
-//extern Font Font6x8(6, 8, Font6x8Pixels, 32, 96);
