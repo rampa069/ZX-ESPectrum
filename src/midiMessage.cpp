4479406a56caa84f9950ff384db60702cfe54a34
jorge.fuertes <jorge@jorgefuertes.com>
2019-05-24 18:55:55 +0200

    Testing 128k

--- src/midiMessage.cpp
+++ src/midiMessage.cpp
@@ -1,14 +1,9 @@
 #include "Sound/midiMessage.h"
 #include "Arduino.h"
 
-
 byte last_status;
 
-void send_channel_message(byte command,
-                          byte channel,
-                          int num_data_bytes,
-                          byte * data)
-{
+void send_channel_message(byte command, byte channel, int num_data_bytes, byte *data) {
     // We are assuming that all of the inputs are in a valid range:
     //  Acceptable commands are 0x80 to 0xF0.
     // Channels must be between 1 and 16.
@@ -23,25 +18,22 @@ void send_channel_message(byte command,
     // Combine MS-nybble of command with channel nybble.
     first = (command & 0xf0) | ((channel - 1) & 0x0f);
 
-    if(first != last_status)
-    {
+    if (first != last_status) {
         Serial.write(first);
         last_status = first;
     }
 
     // Then send the right number of data bytes
-    for(int i = 0; i < num_data_bytes; i++)
-    {
+    for (int i = 0; i < num_data_bytes; i++) {
         Serial.write(data[i]);
     }
 }
 
-void midiMessage(uint8_t message, uint8_t channel, uint8_t data1, uint8_t data2)
-{
-uint8_t data[2];
-  data[0]=data1;
-  data[1]=data2;
+void midiMessage(uint8_t message, uint8_t channel, uint8_t data1, uint8_t data2) {
+    uint8_t data[2];
+    data[0] = data1;
+    data[1] = data2;
 
-
-  //send_channel_message(message,channel,2,data);
+    // send_channel_message(message,channel,2,data);
+    // Serial.printf("MSG: %u\n", data);
 }
