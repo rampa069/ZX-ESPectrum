9d90c31edcbb983f2697f386bdc84e6a2c6ad105
Ramón Martínez <rampa@encomix.org>
2019-07-14 20:34:21 +0200

    added assets directory

---
+++ assets/ay-emulator/flash_25mhz.sh
@@ -0,0 +1,1 @@
+avrdude -p m328p -c USBasp -B4 -v -v -v -v -U flash:w:AY_Emul_247_2ch_m328_ay_speaker.hex -U eeprom:w:Conf_serial_25MHz_1_75Mhz.hex -U lfuse:w:0xee:m -U hfuse:w:0xdf:m -U efuse:w:0xfd:m
